f9983dcfc33880a5d71c2f938f1104e191b54ba0
Joshua Tucker <the1jtux@gmail.com>
2021-03-01 15:01:04 -0500

    UserEntity declared and added to DbContext

--- VoteManager.Data/AppDbContext.cs
+++ VoteManager.Data/AppDbContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using VoteManager.Data.Entities;
 
 namespace VoteManager.Data
 {
@@ -7,6 +8,15 @@ namespace VoteManager.Data
         public AppDbContext(DbContextOptions<AppDbContext> options)
             : base(options) { }
 
+        public DbSet<UserEntity> Users { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserEntity>(entity =>
+            {
+                entity.HasIndex(e => e.Email).IsUnique();
+                entity.HasIndex(e => e.Username).IsUnique();
+            });
+        }
     }
 }
